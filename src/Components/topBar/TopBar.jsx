138874eaf506ba2f071b26587baeed890b880335
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-26 10:39:29 +0200

    Feat: Start the Top bar elements

--- src/Components/topBar/TopBar.jsx
+++ src/Components/topBar/TopBar.jsx
@@ -1,9 +1,38 @@
 import "./topbar.css"
-
+import { Search, Person, Chat, Notifications } from "@material-ui/icons"
 export default function TopBar() {
   return (
-    <div>
-      Top bar
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">DriverCoder</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search />
+          <input placeholder="Search for friends, post or video " className="searchInput"/>
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/Person/2.jpg" alt="profile_pick" className="topbarImg" />
+      </div>
     </div>
   )
 }
