fc03cf02dcf0e708b407c208d02a41c269337eff
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-07-01 11:58:05 +0200

    Feat: Design the right bar

--- src/Components/rightbar/Rightbar.jsx
+++ src/Components/rightbar/Rightbar.jsx
@@ -3,7 +3,25 @@ import "./rightbar.css"
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      Right bar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+           <b>Brigitter Ena</b> and <b>4 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/Person/3.jpg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">John Carter</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
