114a2aac0200fe96de172f05c6fb8e5cb563c982
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-17 20:00:57 +0200

    Feat: Add the like dislike function

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+
 import "./post.css"
 import {MoreVert} from "@material-ui/icons"
 import { users } from "../../dummyData"
 
 export default function Post({post}) {
+  const [like, setLike] = useState(post.like)
+  const [isLiked, setIsLiked] = useState(false)
+
+  const handleLike = () => {
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+  }
   return (
     <div className="post">
       <div className="postWrapper">
@@ -22,9 +31,9 @@ export default function Post({post}) {
         </div>
         <div className="postBottom">
           <div className ="postBottomLeft">
-            <img className="likeIcon" src="/assets/like.png" alt="" />
-            <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="postLikeCounter">{post.like} people liked this</span>
+            <img className="likeIcon" src="/assets/like.png" alt="" onClick={handleLike} />
+            <img className="likeIcon" src="/assets/heart.png" alt="" onClick={handleLike} />
+            <span className="postLikeCounter">{like} people liked this</span>
           </div>
           <div className ="postBottomRight">
             <span className="postcommentText">{post.comment} comments</span>
