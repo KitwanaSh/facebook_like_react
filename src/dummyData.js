e69618d5cac5ee239dd0406ac76e21e6e692d8e5
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-17 13:02:37 +0200

    Feat: Add a dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,75 @@
+export const users = [
+	{
+		id: 1,
+		profilePicture: "assets/Person/1.jpg",
+		userName: "Kitwan Ezechiel"
+	},
+	{
+		id: 2,
+		profilePicture: "assets/Person/2.jpg",
+		userName: "John Snow"
+	},
+	{
+		id: 3,
+		profilePicture: "assets/Person/3.jpg",
+		userName: "Alsad Kamate"
+	},
+	{
+		id: 4,
+		profilePicture: "assets/Person/4.jpg",
+		userName: "Patrick Liza"
+	},
+	{
+		id: 5,
+		profilePicture: "assets/Person/5.jpg",
+		userName: "Elizabeth Keen"
+	}
+];
+
+export const Posts = [
+	{
+		id: 1,
+		desc: "I love my life, No hate with me.",
+		photo: "assets/Post/1.jpg",
+		date: "4 mins ago",
+		userId: 1,
+		like: 43,
+		comment: 10,
+	},
+	{
+		id: 2,
+		desc: "Sun shine today.",
+		photo: "assets/Post/2.jpg",
+		date: "40 mins ago",
+		userId: 2,
+		like: 3,
+		comment: 1,
+	},
+	{
+		id: 3,
+		desc: "I was born in this business.",
+		photo: "assets/Post/3.jpg",
+		date: "36 mins ago",
+		userId: 3,
+		like: 70,
+		comment: 24,
+	},
+	{
+		id: 4,
+		desc: "This is where all started.",
+		photo: "assets/Post/4.jpg",
+		date: "7 days ago",
+		userId: 4,
+		like: 105,
+		comment: 26,
+	},
+	{
+		id: 5,
+		desc: "Let's do this by all the means",
+		photo: "assets/Post/5.jpg",
+		date: "15 mins ago",
+		userId: 5,
+		like: 18,
+		comment: 3,
+	},
+]
