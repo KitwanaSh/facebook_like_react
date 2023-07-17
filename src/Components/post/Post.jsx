f16f3dfa8a421531db374f1b23ff27e84eefd385
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-17 13:29:00 +0200

    Feat: Update

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,32 +1,32 @@
 import "./post.css"
 import {MoreVert} from "@material-ui/icons"
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postLeft">
             <img className="postProfileImg" src="/assets/Person/1.jpg" alt="" />
-            <span className="postUserName">Rodriguez Kitwana</span>
-            <span className="postDate">5mins ago</span>
+            <span className="postUserName">{post.userName}</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">This is my new post.Check that out!</span>
-          <img className="postImg" src="/assets/Post/1.jpg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
         <div className="postBottom">
           <div className ="postBottomLeft">
             <img className="likeIcon" src="/assets/like.png" alt="" />
             <img className="likeIcon" src="/assets/heart.png" alt="" />
-            <span className="postLikeCounter">20 people liked this</span>
+            <span className="postLikeCounter">{post.like} people liked this</span>
           </div>
           <div className ="postBottomRight">
-            <span className="postcommentText">8 comments</span>
+            <span className="postcommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
