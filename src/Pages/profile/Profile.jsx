189f03ccb25e67a849c3a2f3e9a9d0dd93dcba02
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-18 12:17:36 +0200

    Fix: Fit the cover image

--- src/Pages/profile/Profile.jsx
+++ src/Pages/profile/Profile.jsx
@@ -7,26 +7,26 @@ import Feed from '../../Components/feed/Feed'
 export default function Profile() {
 	return (
 		<>
-	    <TopBar />
-	    <div className="profile">
-	    	<Sidebar />
-	    	<div className="profileRight">
-	      		<div className="profileRightTop">
-	      			<div className="profileCover">
-		      			<img src="assets/Post/3.jpg" alt="" class="profileCoverImg" />
-		      			<img src="assets/Person/1.jpg" alt="" class="profileUserImg" />
-	      			</div>
-	      			<div className="profileInfo">
-	      				<h4 className="profileInfoName">The ging dog</h4>
-	      				<span className="profileInfoDesc">The element to discribe th future.</span>
-	      			</div>
-	      		</div>
-	      		<div className="profileRightBottom">
-		      		<Feed />
-		      		<Rightbar />
-	      		</div>
-	      	</div>
-	    </div>
-    </>
+		    <TopBar />
+		    <div className="profile">
+		    	<Sidebar />
+		    	<div className="profileRight">
+		      		<div className="profileRightTop">
+		      			<div className="profileCover">
+			      			<img class="profileCoverImg" src="assets/Post/3.jpg" alt="" />
+			      			<img class="profileUserImg" src="assets/Person/1.jpg" alt="" />
+		      			</div>
+		      			<div className="profileInfo">
+		      				<h4 className="profileInfoName">The ging dog</h4>
+		      				<span className="profileInfoDesc">The element to discribe th future.</span>
+		      			</div>
+		      		</div>
+		      		<div className="profileRightBottom">
+			      		<Feed />
+			      		<Rightbar />
+		      		</div>
+		      	</div>
+		    </div>
+    	</>
 	)
 }
