26c0bf9d7af0269a4d6890759f3457b53e39e4b2
KitwanaSh <rodriguekitweze@gmail.com>
2023-07-19 21:00:16 +0200

    Feat: Start the registration page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './Pages/home/Home';
 import Profile from './Pages/profile/Profile';
 import Login from './Pages/login/Login';
+import Register from './Pages/register/Register';
 
 function App() {
   return <Login />
